fafc69461f6bc8d0b088bfa23ef6fb830a4d4e3c
plannemo <colorlogic@naver.com>
2019-12-10 16:56:13 +0900

    헤더메인메뉴디자인완성

---
+++ js/menu-tab.js
@@ -0,0 +1,14 @@
+var menu = $('.menu');
+var items = $('.menu-item');
+var btns = $('.btn-menu');
+var anchors = $('.sub-menu a')
+
+// 토클 버튼을 클릭했을때 하위 메뉴를 보이도록 설정할 것 
+btns.click(function(e){
+  e.preventDefault();
+  items.removeClass('menu-act');
+  $(this).parent().addClass('menu-act');
+
+});
+
+anchors.addClass('icon-dot-circled');
